199c226b5590f9efb02450cbf8fd60105e99ea4f
peka <you@example.com>
2019-06-05 14:34:32 +0200

    Added java logging to services

--- MoneyTransferServer/src/main/java/com/kramarenko/service/AccountService.java
+++ MoneyTransferServer/src/main/java/com/kramarenko/service/AccountService.java
@@ -7,9 +7,11 @@ import javax.inject.Inject;
 import javax.inject.Singleton;
 import java.util.List;
 import java.util.Optional;
+import java.util.logging.Logger;
 
 @Singleton
 public class AccountService {
+    private static final Logger LOG = Logger.getLogger(AccountService.class.getName());
     private AccountDao dao;
 
     @Inject
@@ -18,14 +20,18 @@ public class AccountService {
     }
 
     public List<Account> getAccounts() {
+        LOG.info("Getting accounts from data storage");
         return dao.getAllAccounts();
     }
 
     public Account createAccount(String name, double amount) {
+        LOG.info("Inserting account with name = " + name + " and amount = " + amount +
+                "to data storage");
         return dao.createAccount(name, amount);
     }
 
     public Optional<Account> getAccountById(int id) {
+        LOG.info("Getting account with id = " + id + " from data storage");
         return dao.getAccountById(id);
     }
 }
--- MoneyTransferServer/src/main/java/com/kramarenko/service/TransferService.java
+++ MoneyTransferServer/src/main/java/com/kramarenko/service/TransferService.java
@@ -9,9 +9,11 @@ import javax.inject.Singleton;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.logging.Logger;
 
 @Singleton
 public class TransferService {
+    private static final Logger LOG = Logger.getLogger(TransferService.class.getName());
     private AccountDao dao;
 
     @Inject
@@ -20,20 +22,25 @@ public class TransferService {
     }
 
     public Result transfer(int from, int to, double amount) {
+        LOG.info("Transfering money = " + amount + "from account = " + from + " to = " + to);
+
         List<String> errors = new ArrayList<>();
 
         Optional<Account> accFrom = dao.getAccountById(from);
         Optional<Account> accTo = dao.getAccountById(to);
 
         if (amount < 0) {
+            LOG.info("Amount is negative");
             errors.add("Amount cannot be negative");
         }
 
         if (accFrom.isEmpty()) {
+            LOG.info("Non existent account with id = " + from);
             errors.add("Non existent account with id = " + from);
         }
 
         if (accTo.isEmpty()) {
+            LOG.info("Non existent account with id = " + to);
             errors.add("Non existent account with id = " + to);
         }
 
@@ -42,6 +49,7 @@ public class TransferService {
         }
 
         if (accFrom.get().getAmount() < amount) {
+            LOG.info("Insufficient funds on account with id = " + from);
             errors.add("Insufficient funds on account with id = " + from);
         }
 
@@ -53,8 +61,11 @@ public class TransferService {
         if (resultFrom.isSuccess()) {
             accTo.get().setAmount(accTo.get().getAmount() + amount);
             dao.updateAccount(to, accTo.get());
+
+            LOG.info("Transfer is successful money = " + amount + "from account = " + from + " to = " + to);
+            return Result.success();
         }
 
-        return Result.success();
+        return Result.failure();
     }
 }
